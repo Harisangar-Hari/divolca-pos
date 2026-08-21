e4714387a0843900cdfc472dc24a816d95d1872e
Harisangar <sangarhari10@gmail.com>
2026-08-21 17:22:39 +0530

    deployed backend url updated

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const api = axios.create({
-    // baseURL: "https://pos-nest-k5pm.onrender.com/api",
-    baseURL: "http://localhost:3005/api",
+    baseURL: "https://divolca-backend.onrender.com/api",
+    // baseURL: "http://localhost:3005/api",
 });
